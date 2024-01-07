5ca609cb8aded24f963d85f2b4dee4899fad74bb
marlon passos <marlon.passos@m3ecommerce.com>
2024-01-07 17:26:47 -0300

    test(styleguide-types): types of emptyState

--- packages/styleguide-types/__tests__/index.spec.tsx
+++ packages/styleguide-types/__tests__/index.spec.tsx
@@ -1,5 +1,7 @@
+import React from 'react';
 import type { AlertProps } from '../src/Alert';
 import type { DropdownProps } from "../src/Dropdown"
+import type { EmptyStateProps } from "../src/EmptyState"
 import { testType } from './test-utils';
 
 // @dts-jest:group Alert
@@ -10,12 +12,12 @@ import { testType } from './test-utils';
     type: 'success',
     action: {
       label: 'label',
-      onClick: () => {}
+      onClick: () => { }
     },
     autoClose: 1000,
     closeIconLabel: 'closeIconLabel',
     focusOnOpen: true,
-    onClose: () => {}
+    onClose: () => { }
   });
 }
 
@@ -23,9 +25,9 @@ import { testType } from './test-utils';
 {
   // @dts-jest:pass:snap
   testType<DropdownProps>({
-    label:"Disabled",
-      disabled: true,
-      options: [
+    label: "Disabled",
+    disabled: true,
+    options: [
       { value: 'visa', label: 'Visa' },
       { value: 'mastercard', label: 'Mastercard' },
       { value: 'elo', label: 'Elo' },
@@ -33,7 +35,18 @@ import { testType } from './test-utils';
       { value: 'giftcard', label: 'Gift Card' },
       { value: 'amex', label: 'American Express' },
     ],
-      value: 'mastercard',
-      onChange: (_, v) => {}
+    value: 'mastercard',
+    onChange: (_, v) => { }
   });
+}
+
+// @dts-jest:group EmptyState
+{
+  // @dts-jest:pass:snap
+  testType<EmptyStateProps>({
+    title: 'title',
+    children: () => <div>children</div>,
+    height: 100,
+    testId: 'testId'
+  })
 }
